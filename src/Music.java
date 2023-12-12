3d9201aa37dd30eb49b867b76846798e0f787b65
Shirin Kydralieva <149779032+shirinkydralieva@users.noreply.github.com>
2023-12-12 19:05:14 +0600

    updated

--- src/Music.java
+++ src/Music.java
@@ -3,26 +3,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class Music {
-    private String title;
+    private String name;
     private String artist;
     private int duration;
-    public Music(String title, String artist, int duration) {
-        this.title = title;
+    public Music(String name, String artist, int duration) {
+        this.name = name;
         this.artist = artist;
         this.duration = duration;
     }
 
     public String getTitle() {
-        return title;
+        return name;
     }
 
     @Override
     public String toString() {
-        return title + " - " + artist + " (" + duration + " сек)";
+        return name + " (artist: " + artist + "), duration: " + duration/60 + " min " + duration%60 + " sec";
     }
 
     public int getDuration() {
-return duration;
+        return duration;
 
     }
 }
--- src/Playlist.java
+++ src/Playlist.java
@@ -1,10 +1,13 @@
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Playlist {
     private static boolean isSubscribed = false;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         int code;
         ArrayList<Music> playlist = new ArrayList<>();
@@ -20,52 +23,195 @@ public class Playlist {
         playlist.add(new Music("Apologize", "OneRepublic", 184));
         playlist.add(new Music("Somebody that I used to know", "Gotye, Kimbra", 244));
 
-        do {
-            System.out.println("\nВыберите действие:");
-            System.out.println("1. Добавить песню");
-            System.out.println("2. Удалить песню");
-            System.out.println("3. Перетасовать плейлист");
-            System.out.println("4. Отсортировать плейлист по названию");
-            System.out.println("5. Показать плейлист и общую продолжительность");
-            System.out.println("6. Найти песню по названию");
-            System.out.println("0. Выйти");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // consume the newline character
-
-            switch (choice) {
-                case 1:
-                    if (isSubscribed || askForSubscription()) {
-                        addMusic(playlist, scanner);
-                    }
-                    break;
-                case 2:
-                    removeMusic(playlist, scanner);
-                    break;
-                case 3:
+        printMenu();
+        try {
+            code = scan.nextInt();
+        } catch (InputMismatchException e){
+            System.err.println("Недопустимый символ! Попробуйте еще раз запустить программу.");
+            code = 0;
+        }
+        while (code != 0) {
+            switch (code) {
+                case 1: {
+                    scan.nextLine();
+                    addToPlaylist(playlist);
+                } break;
+                case 2: {
+                    printPlaylist(playlist);
+                } break;
+                case 3: {
+                    removeFromPlaylist(playlist);
+                } break;
+                case 4: {
                     shufflePlaylist(playlist);
-                    break;
-                case 4:
+                } break;
+                case 5: {
                     sortPlaylistByName(playlist);
-                    break;
-                case 5:
-                    displayMusicAndTotalDuration(playlist);
-                case 6:
-                    findMusicByTitle(playlist, scanner);
-                    break;
-                case 0:
-                    System.out.println("Выход из музыкального плейлиста.");
-                    System.exit(0);
-                default:
-                    System.out.println("Некорректный выбор. Попробуйте снова.");
-                    break;
+                } break;
+                case 6: {
+                    findMusicByTitle(playlist);
+                }  break;
+                case 7: {
+                    startPlaylist(playlist);
+                } break;
+                case 8: {
+                    askForSubscription();
+                } break;
+                default: {
+                    System.out.println("Неверный ввод, попробуйте еще раз");
+                }
             }
-        } while (true);
+            printMenu();
+            try {
+                code = scan.nextInt();
+            } catch (InputMismatchException e){
+                System.err.println("Недопустимый символ!");
+                code = 0; //тут тоже
+            }
+        }
+
     }
 
-    private static void findMusicByTitle(ArrayList<Music> playlist, Scanner scanner) {
+    public static void printMenu(){
+        System.out.println("\n***  MENU  ***" +
+                "\n1. Добавить новую песню" +
+                "\n2. Отобразить список песен" +
+                "\n3. Удалить песню" +
+                "\n4. Перетасовать песни " +
+                "\n5. Отсортировать плейлист по названию " +
+                "\n6. Найти песню по названию " +
+                "\n7. Запустить плейлист " +
+                "\n8. Купить подписку " +
+                "\n0. Выйти");
+    }
+
+    public static void addToPlaylist(ArrayList<Music> playlist){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите название песни: ");
+        String name = scan.nextLine();
+        System.out.print("Введите исполнителя песни: ");
+        String artist = scan.nextLine();
+        System.out.print("Продолжительность песни в секундах: ");
+        try {
+            Integer duration = scan.nextInt();
+            checkNumber(duration);
+            playlist.add(new Music(name, artist, duration));
+            System.out.println("Новая песня " + name + " (" + artist + ") успешно добавлена!");
+        }catch (InputMismatchException e){
+            System.err.println("Недопустимый символ!\nВнимание! Из-за допущенной вами ошибки песня не была добавлена!");
+        } catch (NegativeNumberException e2){
+            System.err.println("Вы ввели отрицательное число!");
+        }
+
+
+    }
+
+    public static void printPlaylist (ArrayList<Music> playlist){
+        int j = 1;
+        int totalDuration = 0;
+        System.out.println("Список песен: ");
+        for (Music i : playlist) {
+            System.out.println(j + ". " + i.toString());
+            j++;
+            totalDuration += i.getDuration();
+        }
+        System.out.println("Количество песен в плейлисте: " + playlist.size());
+        System.out.println("Общая продолжительность песен в плейлисте: " + totalDuration/60 + " min " + totalDuration%60 + " sec");
+    }
+
+    public static void removeFromPlaylist (ArrayList<Music> playlist){
+        int num;
+        System.out.print("Выберите номер песни для удаления: ");
+        try {
+            num = new Scanner(System.in).nextInt();
+            System.out.println("Песня <" + playlist.get(num - 1) + "> успешно удалена!");
+            playlist.remove(num - 1);
+        } catch (InputMismatchException e){
+            System.err.println("Недопустимый символ!\nВнимание! Из-за допущенной вами ошибки песня не была удалена!");
+        } catch (IndexOutOfBoundsException e2){
+            System.err.println("Недопустимый символ!\nВнимание! Из-за допущенной вами ошибки песня не была удалена!");
+        }
+    }
+    public static void shufflePlaylist(ArrayList<Music> playlist){
+        //как показывал ментор, необязательно создавать объект класса рандом с ссылкой, можно работать с ним с помощью просто ключ слова new
+        for (int i = playlist.size() - 1; i > 0; i--) {
+            int j = new Random().nextInt(i + 1); //Вот тут видите :>
+            Music temp = playlist.get(i);
+            playlist.set(i, playlist.get(j));
+            playlist.set(j, temp);
+        }
+        System.out.println("Плейлист перетасован");
+    }
+
+    public static void startPlaylist(ArrayList<Music> playlist){
+        /*
+         * Прошу ко вниманию! Этот метод просто имитирует запуск песен из плейлиста
+         */
+        int index = 0;
+        System.out.println("Перед тем, как плейлист запустится, выберите порядок проигрывания песен: \n<1 - по умолчанию> <2 - вразброс>");
+        try {
+            int num = new Scanner(System.in).nextInt();
+            if (num == 1){
+                while (index < playlist.size()){
+                    playMusic(playlist, index);
+                    index++;
+                }
+            } else if (num == 2) {
+                shufflePlaylist(playlist); //Воспроизведение музыки вразброс
+                while (index < playlist.size()){
+                    playMusic(playlist, index);
+                    index++;
+                }
+            } else {
+                System.out.println("Неверная команда. Вы должны были ввести 1 или 2.");
+            }
+        } catch (InputMismatchException e){
+            System.err.println("Недопустимый символ!\nВнимание! Из-за допущенной вами ошибки плейлист не был запущен!");
+        }
+    }
+
+    //1) Метод вспомогательный к startPlaylist
+    public static void printMusic(Music music){
+        System.out.println("Играет " + music + " ...");
+    }
+
+    //2) Еще один вспомогательный метод во избежание дублирования кода
+    public static void playMusic(ArrayList<Music> playlist, int index){
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.schedule(() -> printMusic(playlist.get(index)), 2, TimeUnit.SECONDS);
+        try {
+            scheduledExecutorService.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        scheduledExecutorService.shutdown();
+    }
+
+
+    public static boolean askForSubscription() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Для прослушивания песен без рекламы вам нужно подписаться на нашу услугу за 5$ в месяц.");
+        System.out.println("Хотите подписаться? (Да / Нет");
+
+        String reaction = scanner.nextLine();
+        while (!"да".equals(reaction) && !"нет".equals(reaction)) {
+            System.out.println("Некорректный ввод. Пожалуйста, введите Да или Нет.");
+            reaction = scanner.nextLine();
+        }
+            if ("да".equals(reaction)) {
+                System.out.println("Спасибо за подписку! Теперь вы можете слушать песни без рекламы.");
+                isSubscribed = true;
+                return true;
+            } else {
+                System.out.println("Вы отказались от подписки. Реклама будет отображаться при прослушивании песен.");
+                return false;
+            }
+        }
+
+    public static void findMusicByTitle(ArrayList<Music> playlist) {
         System.out.println("Введите название песни для поиска:");
-        String titleToFind = scanner.nextLine();
+        String titleToFind = new Scanner(System.in).nextLine();
 
         boolean found = false;
         for (Music music : playlist) {
@@ -76,78 +222,23 @@ public class Playlist {
                 break;
             }
         }
-
         if (!found) {
             System.out.println("Песня с названием '" + titleToFind +  "' не найдена в плейлисте.");
         }
     }
-    private static void addMusic(ArrayList<Music> playlist, Scanner scanner) {
-        System.out.println("Введите название песни:");
-        String title = scanner.nextLine();
-        System.out.println("Введите исполнителя:");
-        String artist = scanner.nextLine();
-        System.out.println("Введите продолжительность песни (в секундах):");
-        int duration = scanner.nextInt();
-        scanner.nextLine(); // consume the newline character
 
-        Music music = new Music(title, artist, duration);
-        playlist.add(music);
-        System.out.println("Песня добавлена в плейлист: " + music.getTitle());
-    }
-
-    private static void removeMusic(ArrayList<Music> playlist, Scanner scanner) {
-        System.out.println("Введите название песни для удаления:");
-        String titleToRemove = scanner.nextLine();
-
-        playlist.removeIf(music -> music.getTitle().equalsIgnoreCase(titleToRemove));
-        System.out.println("Песня удалена из плейлиста: " + titleToRemove);
-    }
-
-    private static void shufflePlaylist(ArrayList<Music> playlist) {
-        Collections.shuffle(playlist);
-        System.out.println("Плейлист перемешан.");
-    }
-
-    private static void sortPlaylistByName(ArrayList<Music> playlist) {
+    public static void sortPlaylistByName(ArrayList<Music> playlist) {
         Collections.sort(playlist, (music1, music2) -> music1.getTitle().compareToIgnoreCase(music2.getTitle()));
-        System.out.println("Плейлист отсортирован по названию песни.");
+        System.out.println("Плейлист отсортирован по названию песни(A-Z).");
     }
 
-    private static boolean displayMusicAndTotalDuration(ArrayList<Music> playlist) {
-        System.out.println("Текущий плейлист:");
-        for (Music music : playlist) {
-            System.out.println(music);
-        }
-
-        int totalDurationInSeconds = playlist.stream().mapToInt(Music::getDuration).sum();
-        int minutes = totalDurationInSeconds / 60;
-        int seconds = totalDurationInSeconds % 60;
-        System.out.println("Общая продолжительность плейлиста: " + minutes + " минут " + seconds + " секунд");
-
-        return false;
+    public static void checkNumber(int number) throws NegativeNumberException {
+        if (number <  0) throw new NegativeNumberException("[number < 0]");
     }
 
-
-    public static boolean askForSubscription() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Для добавления песни вам нужно подписаться на нашу услугу за 5$ в месяц.");
-        System.out.println("Хотите подписаться? (Да / Нет");
-
-        String reaction = scanner.nextLine();
-        while (!"да".equals(reaction) && !"нет".equals(reaction)) {
-            System.out.println("Некорректный ввод. Пожалуйста, введите Да или Нет.");
-            reaction = scanner.nextLine();
-        }
-        if ("да".equals(reaction)) {
-            System.out.println("Спасибо за подписку! Теперь вы можете добавлять песни без рекламы.");
-            isSubscribed = true;
-            return true;
-        } else {
-            System.out.println("Вы отказались от подписки. Реклама будет отображаться при добавлении песен.");
-            return false;
-
-
-        }
-
-    }
 }
+
+
+
+
+
